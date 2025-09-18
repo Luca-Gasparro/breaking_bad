530ce97996ddab978744b9b0702f2c621094451f
Luca-Remote <lucagasparro12@gmail.com>
2025-09-18 19:54:05 +0100

    Centre of mass extraction of API
    Function to extract COM of API after an equilibration start time.
    
    Ensured this function could be imported and ran in a different script.
    
    It is likely more features will be saved to this file overtime
    upon realisation of being needed for subsequent functions.

--- rdf/rdf_analysis.py
+++ rdf/rdf_analysis.py
@@ -1,22 +1,17 @@
 import MDAnalysis as mda
 from MDAnalysis.analysis.rdf import InterRDF
 import matplotlib.pyplot as plt
+import sys
+import os
 
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-def rdf_api(topology, trajectory, api_residue_name):
-    """Computes the average radial distribution of the API using MDAnalysis"""
+from utility import api_com_extractor
 
-    # Select the API atoms
-    u = mda.Universe(topology, trajectory)
-    api = u.select_atoms(f"resname {api_residue_name}")
-
-    # Compute the average radial distriution function between API molecules and plot
-    rdf_api = InterRDF(api, api, nbins=50, range=(0.0, 10.0))
-    rdf_api.run()
-    plt.plot(rdf_api.results.bins, rdf_api.results.rdf)
-    plt.savefig("api_rdf.png", dpi=300)
-
-    return
-
-
-rdf_api("dry_nvt.tpr", "dry_nvt_trim.xtc", "NAP")
+api_com_extractor(
+    topology_file="dry_nvt.tpr",
+    trajectroy_file="dry_nvt_trim.xtc",
+    api_residue_name="NAP",
+    output_filename="test",
+    start_time=2000,
+)
---
+++ utility.py
@@ -0,0 +1,41 @@
+# Utility functions
+import os
+import numpy as np
+import MDAnalysis as mda
+
+
+def api_com_extractor(
+    topology_file, trajectroy_file, api_residue_name, output_filename, start_time
+):
+    """Extract centres-of-mass of the API from the time of equilibration starting."""
+
+    # Make sure the output has a .npz extension
+    if not output_filename.endswith(".npz"):
+        output_filename += ".npz"
+
+    # Check for existence of filename. If it exists, skip the recalculation
+    if os.path.exists(output_filename):
+        print(f"API COM file {output_filename} already exists, skipping extraction.")
+        return output_filename
+
+    # Use MDAnalysis to load in the trajectories
+    traj = mda.Universe(topology_file, trajectroy_file)
+    timestep = traj.trajectory.dt
+    start_frame = int(start_time / timestep)
+    number_of_frames = len(traj.trajectory) - start_frame
+
+    # Select residues of API
+    api = traj.select_atoms(f"resname {api_residue_name}")
+    api_resids = sorted(set(api.resids))
+    number_of_api = len(api_resids)
+
+    # COM extraction
+    api_coms = np.zeros((number_of_frames, number_of_api, 3))
+    for i, _ in enumerate(traj.trajectory[start_frame:]):
+        for j, residue in enumerate(api_resids):
+            mol = traj.select_atoms(f"resname {api_residue_name} and resid {residue}")
+            api_coms[i, j] = mol.center_of_mass()
+
+    # Save the results to a file to avoid repeat computation
+    np.savez_compressed(output_filename, api_coms=api_coms)
+    return
