b08cbe5d90abb50ad3b89379572a3c0a06c25cce
Luca-Remote <lucagasparro12@gmail.com>
2025-11-04 15:50:34 +0000

    Changes to how scripts are run
    Now __init__.py is working, I have sorted how scripts run.
    Now we dont't need to duplicate trajectories into the same directories
    as we can use paths to see where one set of trajectories are.
    Will be useful when I need the trajectories for the rdf calculations

--- sim_analysis/energy_min/energy_minimisation_analysis.py
+++ sim_analysis/energy_min/energy_minimisation_analysis.py
@@ -5,45 +5,45 @@ from modules import analysis
 
 # 100 angstrom configuration...
 analysis.energy_minimisation_pe(
-    polymer_xvg="/storage/chem/phuqdw/breaking_bad/sim_analysis/energy_min/em_pvp_relax_100ang.xvg",
-    api_polymer_xvg="/storage/chem/phuqdw/breaking_bad/sim_analysis/energy_min/em_nap_pvp_relax_100ang.xvg",
-    wet_api_polymer_xvg="/storage/chem/phuqdw/breaking_bad/sim_analysis/energy_min/em_wet_nap_pvp_relax_100ang.xvg",
+    polymer_xvg="/storage/chem/phuqdw/breaking_bad/gromacs/em_files/em_pvp_relax_100ang.xvg",
+    api_polymer_xvg="/storage/chem/phuqdw/breaking_bad/gromacs/em_files/em_nap_pvp_relax_100ang.xvg",
+    wet_api_polymer_xvg="/storage/chem/phuqdw/breaking_bad/gromacs/em_files/em_wet_nap_pvp_relax_100ang.xvg",
     box_size=100,
     output_dir="/storage/chem/phuqdw/breaking_bad/sim_analysis/energy_min",
 )
 
 # 110 angstrom configuration
 analysis.energy_minimisation_pe(
-    polymer_xvg="/storage/chem/phuqdw/breaking_bad/sim_analysis/energy_min/em_pvp_relax_110ang.xvg",
-    api_polymer_xvg="/storage/chem/phuqdw/breaking_bad/sim_analysis/energy_min/em_nap_pvp_relax_110ang.xvg",
-    wet_api_polymer_xvg="/storage/chem/phuqdw/breaking_bad/sim_analysis/energy_min/em_wet_nap_pvp_relax_110ang.xvg",
+    polymer_xvg="/storage/chem/phuqdw/breaking_bad/gromacs/em_files/em_pvp_relax_110ang.xvg",
+    api_polymer_xvg="/storage/chem/phuqdw/breaking_bad/gromacs/em_files/em_nap_pvp_relax_110ang.xvg",
+    wet_api_polymer_xvg="/storage/chem/phuqdw/breaking_bad/gromacs/em_files/em_wet_nap_pvp_relax_110ang.xvg",
     box_size=110,
     output_dir="/storage/chem/phuqdw/breaking_bad/sim_analysis/energy_min",
 )
 
 # 130 angstrom configuration
 analysis.energy_minimisation_pe(
-    polymer_xvg="/storage/chem/phuqdw/breaking_bad/sim_analysis/energy_min/em_pvp_relax_130ang.xvg",
-    api_polymer_xvg="/storage/chem/phuqdw/breaking_bad/sim_analysis/energy_min/em_nap_pvp_relax_130ang.xvg",
-    wet_api_polymer_xvg="/storage/chem/phuqdw/breaking_bad/sim_analysis/energy_min/em_wet_nap_pvp_relax_130ang.xvg",
+    polymer_xvg="/storage/chem/phuqdw/breaking_bad/gromacs/em_files/em_pvp_relax_130ang.xvg",
+    api_polymer_xvg="/storage/chem/phuqdw/breaking_bad/gromacs/em_files/em_nap_pvp_relax_130ang.xvg",
+    wet_api_polymer_xvg="/storage/chem/phuqdw/breaking_bad/gromacs/em_files/em_wet_nap_pvp_relax_130ang.xvg",
     box_size=130,
     output_dir="/storage/chem/phuqdw/breaking_bad/sim_analysis/energy_min",
 )
 
 # 140 angstrom configuration
 analysis.energy_minimisation_pe(
-    polymer_xvg="/storage/chem/phuqdw/breaking_bad/sim_analysis/energy_min/em_pvp_relax_140ang.xvg",
-    api_polymer_xvg="/storage/chem/phuqdw/breaking_bad/sim_analysis/energy_min/em_nap_pvp_relax_140ang.xvg",
-    wet_api_polymer_xvg="/storage/chem/phuqdw/breaking_bad/sim_analysis/energy_min/em_wet_nap_pvp_relax_140ang.xvg",
+    polymer_xvg="/storage/chem/phuqdw/breaking_bad/gromacs/em_files/em_pvp_relax_140ang.xvg",
+    api_polymer_xvg="/storage/chem/phuqdw/breaking_bad/gromacs/em_files/em_nap_pvp_relax_140ang.xvg",
+    wet_api_polymer_xvg="/storage/chem/phuqdw/breaking_bad/gromacs/em_files/em_wet_nap_pvp_relax_140ang.xvg",
     box_size=140,
     output_dir="/storage/chem/phuqdw/breaking_bad/sim_analysis/energy_min",
 )
 
 # 160 angstrom configuration
 analysis.energy_minimisation_pe(
-    polymer_xvg="/storage/chem/phuqdw/breaking_bad/sim_analysis/energy_min/em_pvp_relax_160ang.xvg",
-    api_polymer_xvg="/storage/chem/phuqdw/breaking_bad/sim_analysis/energy_min/em_nap_pvp_relax_160ang.xvg",
-    wet_api_polymer_xvg="/storage/chem/phuqdw/breaking_bad/sim_analysis/energy_min/em_wet_nap_pvp_relax_160ang.xvg",
+    polymer_xvg="/storage/chem/phuqdw/breaking_bad/gromacs/em_files/em_pvp_relax_160ang.xvg",
+    api_polymer_xvg="/storage/chem/phuqdw/breaking_bad/gromacs/em_files/em_nap_pvp_relax_160ang.xvg",
+    wet_api_polymer_xvg="/storage/chem/phuqdw/breaking_bad/gromacs/em_files/em_wet_nap_pvp_relax_160ang.xvg",
     box_size=160,
     output_dir="/storage/chem/phuqdw/breaking_bad/sim_analysis/energy_min",
 )
